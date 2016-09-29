eb8167326550c4f13bbe59d94ad15b2e2f5b8718
svirpav <pavel.automation@gmail.com>
2016-09-28 22:12:58 -0700

    Optimization and bug fixing for converter completed

--- main.c
+++ main.c
@@ -4,17 +4,18 @@ int main(int argc, char const *argv[]) {
 
   //Methods scope defention
   void converter();
+  //void debuger();
 
   //Menu Strings
   char menu_intem_header [] = "Menu";
   char menu_intem_sellection [] = "Enter menu item number and press [ENTER]";
   char menu_intem_1 [] = "1. C intruduction: practical test convert inch to catimeters";
+  //char menu_intem_2 [] = "0. Play are debuuger";
   //code variables
-  int simb;
+  char simb;
 
   //Main code implementation
   while(simb != 'e'){
-    simb = -1;
     printf("\e[1;1H\e[2J");
     printf("%s\n", menu_intem_header);
     printf("\n");
@@ -23,7 +24,6 @@ int main(int argc, char const *argv[]) {
     printf("%s", menu_intem_sellection);
     printf(":");
     simb = getchar();
-    printf("%c\n", simb );
     if(simb == '1'){
       converter();
     }
@@ -34,6 +34,7 @@ int main(int argc, char const *argv[]) {
 
 void converter() {
 
+  void flush();
   //Item Strings
   char item_01_header [] = "Convert application";
   char item_01_description [] = "(inch -> cm) Pease enter value :";
@@ -50,7 +51,7 @@ void converter() {
 
   //item_01_ code implementation
   while (item_01_simb != 'e') {
-    item_01_simb = -1;
+    item_01_simb = '0';
     printf("\e[1;1H\e[2J");
     printf("%s\n", item_01_header);
     printf("\n%s", item_01_description);
@@ -58,15 +59,19 @@ void converter() {
     result = input_value * conv_value;
     printf("%s %f\n", inches, input_value );
     printf("%s %f\n", cm, result);
+    flush();
 
     while (item_01_simb != 'r') {
-      item_01_simb = -1;
       printf("%s", item_01_exitpromt);
-      getchar();
       item_01_simb = getchar();
-      printf("%c\n", item_01_simb );
       if (item_01_simb == 'e') break;
+      flush();
     }
 
   }
+}
+
+void flush() {
+  int fl;
+  while ((fl = getchar()) != '\n' && fl != EOF) {}
 }
