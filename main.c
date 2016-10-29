27818a6c2376e80276a14269b03450c8492e69db
svirpav <pavel.automation@gmail.com>
2016-10-29 12:00:25 -0700

    Implemented method "restricted" words.

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
+//Methods declarartions
+void flush(void);
+void clear(void);
+void converter(void);
+void ident_names(void);
+void reserved_words();
+
 int main(int argc, char const *argv[]) {
 
-  //Methods declarations
-  void converter();
-  void ident_names();
-  void clear();
 
   //Menu Strings
   char menu_intem_header [] = "Menu";
   char menu_intem_sellection [] = "Enter menu item number and press [ENTER]";
   char menu_intem_1 [] = "1. C intruduction: practical test convert inch to catimeters";
-  char menu_intem_2 [] = "2.1 - Allowed and Not Allowed identifyer names";
+  char menu_intem_2 [] = "2 - Allowed and Not Allowed identifyer names";
+  char menu_intem_3 [] = "3 - Key and Reserved words in C";
 
 
   //Code variables ddeclarartion
@@ -24,6 +28,7 @@ int main(int argc, char const *argv[]) {
     printf("\n");
     printf("%s\n", menu_intem_1);
     printf("%s\n", menu_intem_2);
+    printf("%s\n", menu_intem_3);
 
     printf("%s", menu_intem_sellection);
     printf(":");
@@ -31,8 +36,11 @@ int main(int argc, char const *argv[]) {
     if(simb == '1'){
       converter();
     }
-    if (simb == '2') {
+    if(simb == '2') {
       ident_names();
+    }
+    if(simb == '3'){
+      reserved_words();
     }
   }
 
@@ -40,11 +48,7 @@ int main(int argc, char const *argv[]) {
 }
 
 //Mathod to conver Inch -> CM. Menu item 1.0
-void converter() {
-
-//Methods declarartions
-  void flush();
-  void clear();
+void converter(void) {
 
   //Strings declarartions
   char item_01_header [] = "Convert application";
@@ -84,11 +88,7 @@ void converter() {
 }
 
 //Method for Allowed/Not Allowed names. Menu item 2.1
-void ident_names(){
-
-//Methods declarartion
-  void flush();
-  void clear();
+void ident_names(void){
 
 //code variabeles declarartion
   char *aNameHeader[0];
@@ -136,14 +136,88 @@ printf("%s\t%s\n\n", aNameHeader[0], naNameHeader[0]);
   getchar();
 }
 
+//Key/Reserved words method implementation
+void reserved_words(void){
+
+//Variables declarartion
+  char key_header[] = "Key words in C, restricted to use.";
+  char *rWords[43];
+
+  int size;
+  int step;
+  int column;
+
+  rWords[0] = "auto";
+  rWords[1] = "break";
+  rWords[2] = "case";
+  rWords[3] = "char";
+  rWords[4] = "const";
+  rWords[5] = "continue";
+  rWords[6] = "default";
+  rWords[7] = "do";
+  rWords[8] = "double";
+  rWords[9] = "else";
+  rWords[10] = "enum";
+  rWords[11] = "extern";
+  rWords[12] = "float";
+  rWords[13] = "for";
+  rWords[14] = "goto";
+  rWords[15] = "if";
+  rWords[16] = "inline";
+  rWords[17] = "int";
+  rWords[18] = "long";
+  rWords[19] = "register";
+  rWords[20] = "return";
+  rWords[21] = "restrict";
+  rWords[22] = "short";
+  rWords[23] = "signed";
+  rWords[24] = "sizeof";
+  rWords[25] = "static";
+  rWords[26] = "struct";
+  rWords[27] = "switch";
+  rWords[28] = "typeof";
+  rWords[29] = "union";
+  rWords[30] = "unsigned";
+  rWords[31] = "void";
+  rWords[32] = "volatile";
+  rWords[33] = "while";
+  rWords[34] = "_Alignas";
+  rWords[35] = "_Alignof";
+  rWords[36] = "_Bool";
+  rWords[37] = "_Complex";
+  rWords[38] = "_Generic";
+  rWords[39] = "_Imaginary";
+  rWords[40] = "_Noreturn";
+  rWords[41] = "_Static_asset";
+  rWords[42] = "#_Thread_local";
+
+  size = sizeof(rWords) / sizeof(rWords[0]);
+
+  step = 0;
+  column = 0;
+
+  while (step < size) {
+
+    printf("%s\t", rWords[step]);
+    if (column == 5) {
+      printf("\n");
+      column = 0;
+    }
+    column++;
+    step++;
+  }
+
+  flush();
+  getchar();
+}
 
 //Flufh getchar() buffer;
-void flush() {
+void flush(void){
   int fl;
   while ((fl = getchar()) != '\n' && fl != EOF) {}
 }
 
 //Clear screen Ubix.
-void clear(){
+void clear(void){
   printf("\e[1;1H\e[2J");
 }
